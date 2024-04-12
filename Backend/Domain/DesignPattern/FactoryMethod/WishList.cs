9e684fd91c9d5f417f9666335379f9253bedc192
ASigness <angelasignes17@gmail.com>
2024-04-12 11:36:49 +0200

    Creada la factory method

---
+++ Backend/Domain/DesignPattern/FactoryMethod/WishList.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Repositories;
+using Backend.Services;
+using SmartTrade.Models;
+
+namespace Backend.Domain.DesignPattern.FactoryMethod
+{
+    public class WishListFactory : ListFactory
+    {
+        public override List AddProduct(Product product)
+        {
+            var dbContext = AppServices.CreateDbContext();
+            return new WishList(new WishListService(new WishListRepository(dbContext)));
+        }
+        public override List DeleteProduct(Product product)
+        {
+            var dbContext = AppServices.CreateDbContext();
+            return new WishList(new WishListService(new WishListRepository(dbContext)));
+        }
+    }
+}
