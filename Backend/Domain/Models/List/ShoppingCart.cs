0ab9d8581ff337a4ad3f2eaeef6c9903734e50e1
Leonardo Sánchez <leoyotrosanimales@gmail.com>
2024-04-23 18:24:02 +0200

    Añadido shoppingcart

---
+++ Backend/Domain/Models/List/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartTrade.Models
+{
+    public partial class ShoppingCart : List
+    {
+        [ForeignKey("Client")]
+        public string ClientEmail { get; set; }
+        public virtual Client Client { get; set; }
+
+        public ShoppingCart() : base()
+        {
+            Name = "ShoppingCart";
+        }
+    }
+}
